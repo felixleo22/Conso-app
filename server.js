cef80b9599891f14696f350c69e2094b29bf8294
apinot <antoine.pinot1@gmail.com>
2020-01-07 16:43:42 +0100

    running app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world !');
+    res.send('Bienvenue sur la page Conso App');
 });
 
 app.listen(8080, () => {
