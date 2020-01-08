c99ae408ebadffd31a7e6f4ae1f4b6ccd1c0ba40
apinot <antoine.pinot1@gmail.com>
2020-01-08 11:47:56 +0100

    installation de twig

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Bienvenue sur Conso App !');
+    res.render('home.html.twig');
 });
 
 router.get('/zinzin', (req, res) => {
