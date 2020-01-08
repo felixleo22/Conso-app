a800888b421bd796d471248be21b5954d2bb505f
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2020-01-08 10:31:32 +0100

    ajout du scan

--- client/index.js
+++ client/index.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.send('Bienvenue sur Conso App !');
 });
 
+router.get('/zinzin', (req, res) => {
+    res.sendFile('./client/views/index.html', { root: '.' });
+});
+
 module.exports = router;
