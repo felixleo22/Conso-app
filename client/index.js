2f7e0edb865978015712124ff13219dc269bb553
Yann-LeLan <yannoulla.Officiel@outlook.fr>
2020-01-08 14:51:15 +0100

    css begins

--- client/index.js
+++ client/index.js
@@ -12,4 +12,12 @@ router.get('/accueil', (req, res) => {
     res.sendFile('./client/views/accueil.html', { root: '.' });
 });
 
+router.get('/connexion', (req, res) => {
+    res.sendFile('./client/views/connexion.html', { root: '.' });
+});
+
+router.get('/inscription', (req, res) => {
+    res.sendFile('./client/views/inscription.html', { root: '.' });
+});
+
 module.exports = router;
