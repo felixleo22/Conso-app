0a48dc822d428ebfc02a78f234323544cee33e81
apinot <antoine.pinot1@gmail.com>
2020-01-28 14:41:28 +0100

    add mongo-express

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
     <h1>Conso App</h1>
-    <p>Une application de projet tutoré CIASIE de comparasion de liste de course géolohellcalisée</p>
+    <p>
+      Une application de projet tutoré
+        CIASIE de comparasion de liste de course géolohellcalisée
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
 };
