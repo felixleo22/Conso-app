e7ef439cee956b38d18bec19b066e7ee849f1dc0
apinot <antoine.pinot1@gmail.com>
2020-02-03 11:55:38 +0100

    hash password with salt

--- api/models/User.js
+++ api/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const user = new mongoose.Schema({
-    eamil: { type: String, required: true},
-    password: { type: String, required: true},
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 }, { versionKey: false });
 
 module.exports = mongoose.model('user', user);
--- api/routers/RouterUsers.js
+++ api/routers/RouterUsers.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
-const crypto = require('crypto');
-
 
 const User = require('../models/User');
+const Password = require('../utils/Password');
 
-router.post('/signin', (req, res) => {
+
+router.post('/user', (req, res) => {
     const { email, password1, password2 } = req.body;
 
     User.findOne({ email }, (err, user) => {
-        if (err) throw err;
-
         if (user) {
-            return res.status(400).json(({ type: 'error', code: 400, message: 'Email already used' }));
+            res.status(400).json(({ type: 'error', code: 400, message: 'Email already used' }));
+            return;
         }
 
         if (password1 !== password2) {
-            return res.status(400).json(({ type: 'error', code: 400, message: 'Password do not match' }));
+            res.status(400).json(({ type: 'error', code: 400, message: 'Password do not match' }));
+            return;
         }
 
         const account = new User({
             email,
-            password: crypto.createHmac('sha512', password1).update('10a3dfcce237614807a9593a7ab95976').digest('hex'),
+            password: Password.hash(password1),
         });
-
+        
         account.save((err2) => {
-            if (err2) throw err;
-            return res.status(201).json({ _id: account.id, email: account.email });
+            if (err2) throw err2;
+            res.status(201).json({ _id: account.id, email: account.email });
         });
     });
 });
