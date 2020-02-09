bc677f27ebf6d64bc9513890da484150699425ab
apinot <antoine.pinot1@gmail.com>
2020-02-09 22:55:08 +0100

    separate store in modules

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,54 +3,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import Axios from 'axios';
 
+/* Modules */
+import AuthModule from './modules/Auth';
+
 Axios.defaults.baseURL = `//${location.hostname}:8080`;
 Vue.prototype.$http = Axios;
 Vue.use(Vuex);
 
+
 export default new Vuex.Store({
-  state: {
-    token: localStorage.getItem('access_token') || null,
-  },
-  getters: {
-    loggedIn(state) {
-      return !!state.token;
-    },
-  },
-  mutations: {
-    retrieveToken(state, token) {
-      state.token = token;
-    },
-    destroyToken(state) {
-      state.token = null;
-    },
-  },
-  actions: {
-    retrieveToken(context, credentials) {
-      return new Promise((resolve, reject) => {
-        Axios.post('/login', {
-          email: credentials.email,
-          password: credentials.password,
-        })
-          .then((response) => {
-            const { token } = response.data;
-            localStorage.setItem('access_token', token);
-            context.commit('retrieveToken', token);
-            resolve(response);
-          })
-          .catch((error) => {
-            const { response } = error;
-            reject(response);
-          });
-      });
-    },
-    destroyToken(context) {
-      return new Promise((resolve) => {
-        if (context.getters.loggedIn) {
-          localStorage.removeItem('access_token');
-          context.commit('destroyToken');
-          resolve();
-        }
-      });
-    },
+  modules: {
+    auth: AuthModule(Axios),
   },
 });
--- client/src/views/LogIn.vue
+++ client/src/views/LogIn.vue
@@ -3,7 +3,7 @@
     <form @submit.prevent="login">
       <h1>Login in</h1>
       <label>Email</label>
-      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="email" type="text" placeholder="Email" autofocus/>
       <label>Mot de passe</label>
       <input v-model="password" type="password" placeholder="Mot de passe" />
       <hr />
