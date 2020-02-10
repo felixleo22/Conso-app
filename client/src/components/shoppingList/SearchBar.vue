fc3e70f112faac56f76ceb4a3c30b43671252a8c
apinot <antoine.pinot1@gmail.com>
2020-02-10 22:55:28 +0100

    default quantity to 1 (remove quantity input in add field)

--- client/src/components/shoppingList/SearchBar.vue
+++ client/src/components/shoppingList/SearchBar.vue
@@ -5,9 +5,6 @@
     <div v-for="item in result" :key="item.name">
       <p @click="add(item)">{{item.name}} </p>
     </div>
-    <label>Quantité</label>
-    <input v-model="quantity" type="number" name="quantity" min="1" required />
-    <button v-on:click="add">Ajouter</button>
 </div>
 </template>
 
@@ -17,24 +14,20 @@ export default {
   data() {
     return {
       name: '',
-      quantity: '1',
       result: [],
     };
   },
   methods: {
     add(item) {
-      console.log(item.codebar);
       const res = {
         codebar: item.codebar,
         name: item.name,
-        quantity: this.quantity,
+        quantity: 1,
       };
       this.$store.dispatch('addItem', res);
     },
     getResult() {
-      console.log(this.name);
       this.$http.get(`/products?search=${encodeURI(this.name)}`).then((response) => {
-        console.log(response.data);
         this.result = response.data;
       });
     },
