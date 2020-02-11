1bf09e53f59be9868a15aaaec3956537d922f4e7
apinot <antoine.pinot1@gmail.com>
2020-02-11 10:42:06 +0100

    install vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <v-app id="app">
+    <v-navigation-drawer app clipped v-model="drawer">
+      <v-list>
+        <v-list-item>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="red accent-4" dark clipped-left>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+          <v-icon>fa-bars</v-icon>
+        </v-app-bar-nav-icon>
+        <v-toolbar-title class="font-weight-bold">Conso App</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <template v-if="loggedIn">
         <router-link :to="{name : 'scan'}">Scan</router-link> |
@@ -12,13 +26,22 @@
         <router-link :to="{name : 'login'}">Connexion</router-link> |
         <router-link :to="{name : 'signin'}">Inscription</router-link> |
       </template>
-    </div>
-    <router-view/>
-  </div>
+    </div> -->
+    <v-content>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      drawer: null,
+    };
+  },
   computed: {
     loggedIn() {
       return this.$store.getters.loggedIn;
