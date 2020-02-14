b6b8466572851fd900da1f26f4cefe88b8b5ed8d
apinot <antoine.pinot1@gmail.com>
2020-02-14 10:55:04 +0100

    scanner toolbar

--- client/src/views/Scan.vue
+++ client/src/views/Scan.vue
@@ -2,19 +2,25 @@
     <div id="scan">
         <shop-selector v-if="!shop"></shop-selector>
         <template v-else>
-          <v-toolbar flat dense width="100%">
-              Shop name
+          <v-toolbar flat dense width="100%" style="border-bottom: 1px solid #ffcdd2">
+              <v-icon left>fa-map-marker-alt</v-icon>
+              <div class="text-truncate" style="padding-right: 1em;">
+                <div style="margin-bottom: -0.5em">{{ this.shop.name }}</div>
+                <small>{{ this.shop.address }}</small>
+              </div>
             <v-spacer></v-spacer>
-            <v-btn @click="deselectShop">Changer</v-btn>
+            <v-btn @click="deselectShop" outlined color="red accent-4">Changer</v-btn>
           </v-toolbar>
-          <scanner @scanned="onProductScanned" v-if="showScanner"></scanner>
-          <price-setter v-else
-            :product="product"
-            :shop="shop"
-            @canceled="onPriceCancel"
-            @updated="onPriceUpdated"
-            @errored="onPriceUpdateError">
-          </price-setter>
+          <v-container>
+            <scanner @scanned="onProductScanned" v-if="showScanner"></scanner>
+            <price-setter v-else
+              :product="product"
+              :shop="shop"
+              @canceled="onPriceCancel"
+              @updated="onPriceUpdated"
+              @errored="onPriceUpdateError">
+            </price-setter>
+          </v-container>
         </template>
     </div>
 </template>
