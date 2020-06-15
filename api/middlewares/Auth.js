a43a7d7e0f1243e202bca836177d01f45fcec709
felixleo22 <leofelixoff@outlook.fr>
2020-06-15 20:05:42 +0200

    test webhook

--- api/middlewares/Auth.js
+++ api/middlewares/Auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+// middlewares
 module.exports = (req, res, next) => {
     // verify if header contains token
     if (!req.headers.authorization) {
